b3cc9fd4bd2c6e0776ffbd41a5c5d61c8a05ffb0
Denis-92 <d.mozenic@gmail.com>
2022-10-13 15:52:27 +0200

    init

---
+++ index.php
@@ -0,0 +1,7 @@
+<h1>LISTA DEGLI ESERCIZI SNACKS</h1>
+
+<h2>
+    <a href="./snacks/snack1.php">
+        ESERCIZIO SNACK 1
+    </a>
+</h2>
---
+++ snacks/snack1.php
@@ -0,0 +1,6 @@
+<h1>ESERCIZIO SNACK 1</h1>
+<h2>
+    <a href="../index.php">
+        TORNA INDIETRO
+    </a>
+</h2>
