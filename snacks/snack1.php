98f06d7f39c24e21f8f0d0c28751821fa63da521
Denis-92 <d.mozenic@gmail.com>
2022-10-13 16:18:51 +0200

    display 1 basket game

--- snacks/snack1.php
+++ snacks/snack1.php
@@ -4,3 +4,52 @@
         TORNA INDIETRO
     </a>
 </h2>
+
+<!-- 
+    Snack 1
+    Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+
+    Ogni array avrà una squadra di casa e una squadra ospite,
+        punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+
+    Stampiamo a schermo tutte le partite con questo schema.
+    Olimpia Milano - Cantù | 55-60
+ -->
+
+ <?php
+
+ $partiteBasket = [
+        $partita_venerdi = [
+            'squadraCasa' => 'Milano',
+            'squadraOspite' => 'Roma',
+            'puntiCasa' => '55',
+            'puntiOspite' => '60'
+        ],
+
+        $partita_sabato_1 = [
+            'squadraCasa' => 'Venezia',
+            'squadraOspite' => 'Napoli',
+            'puntiCasa' => '36',
+            'puntiOspite' => '32'
+        ],
+
+        $partita_sabato_2 = [
+            'squadraCasa' => 'Imola',
+            'squadraOspite' => 'Monza',
+            'puntiCasa' => '95',
+            'puntiOspite' => '56'
+        ],
+
+        $partita_domenica = [
+            'squadraCasa' => 'Bari',
+            'squadraOspite' => 'Cagliari',
+            'puntiCasa' => '99',
+            'puntiOspite' => '99'
+        ]
+    ];
+
+echo $partiteBasket[0]['squadraCasa'] . " - ";
+echo $partiteBasket[0]['squadraOspite'] . " | ";
+echo $partiteBasket[0]['puntiCasa'] . "-";
+echo $partiteBasket[0]['puntiOspite'];
+ ?>
